bdc42f45b6d41b54e226e504d8c7b4e3590360a2
innerlee <363664470@qq.com>
2015-10-09 09:47:43 +0800

    remove dependence on RDatasets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,15 @@
-using LIBLINEAR
-using Base.Test
+using LIBSVM
+iris = readcsv(joinpath(dirname(@__FILE__), "iris.csv"))
+labels = iris[:, 5]
+instances = convert(Matrix{Float64}, iris[:, 1:4]')
+model = svmtrain(labels[1:2:end], instances[:, 1:2:end]; verbose=true)
+gc()
+(class, decvalues) = svmpredict(model, instances[:, 2:2:end])
+correct = Bool[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+@assert (class .== labels[2:2:end]) == correct
 
-
-using RDatasets
-
-# Load Fisher's classic iris data
-iris = dataset("datasets", "iris")
-
-# LIBLINEAR handles multi-class data automatically using a one-against-one strategy
-labels = iris[:Species]
-
-# First dimension of input data is features; second is instances
-instances = convert(Array,iris[:, 1:4])'
-
-# Train model on half of the data using default parameters. See the linear_train
-# function in LIBLINEAR.jl for optional parameter settings.
-model = linear_train(labels[1:2:end], instances[:, 1:2:end], verbose=true);
-
-# Test model on the other half of the data.
-(predicted_labels, decision_values) = linear_predict(model, instances[:, 2:2:end]);
-
-# Compute accuracy
-@printf "Accuracy: %.2f%%\n" mean((predicted_labels .== labels[2:2:end]))*100
+model = svmtrain(labels[1:2:end], sparse(instances[:, 1:2:end]); verbose=true)
+gc()
+(class, decvalues) = svmpredict(model, sparse(instances[:, 2:2:end]))
+correct = Bool[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+@assert (class .== labels[2:2:end]) == correct
