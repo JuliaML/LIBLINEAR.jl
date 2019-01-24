9766278d9380c82eb07f2465da2a0cfcb8c0bb55
jzazo <jzazo@g.harvard.edu>
2019-01-24 13:26:09 -0500

    Adapt tests in branch #weights.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,9 @@ using SparseArrays
 iris = readdlm(joinpath(dirname(@__FILE__), "iris.csv"), ',')
 labels = iris[:, 5]
 instances = convert(Matrix{Float64}, iris[:, 1:4]')
+W = ones(length(labels))
 
-model = linear_train(labels[1:2:end], instances[:, 1:2:end]; verbose=true, solver_type=Cint(0))
+model = linear_train(labels[1:2:end], instances[:, 1:2:end]; W=W, verbose=true, solver_type=Cint(0))
 GC.gc()
 (class, decvalues) = linear_predict(model, instances[:, 2:2:end], verbose=true)
 correct = Bool[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
