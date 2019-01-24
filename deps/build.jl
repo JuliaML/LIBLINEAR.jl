173eb042c7ec033c4657191c0986360f05a2a2eb
jzazo <jzazo@g.harvard.edu>
2019-01-24 13:06:49 -0500

    Build sources from standard liblinear and liblinear-weights.

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,14 @@
 if !Sys.iswindows()
-  cd(joinpath(dirname(@__FILE__), "liblinear-221"))
+  # Build standard library
+  cd(joinpath(@__DIR__, "liblinear-221"))
   run(`make clean`)
   run(`make lib`)
   run(`cp liblinear.so.3 ../liblinear.so.3`)
+
+  # Build weights mod
+  cd(joinpath(@__DIR__, "liblinear-weights-221"))
+  run(`make clean`)
+  run(`make lib`)
+  rm("../liblinear-weights.so.3", force=true, recursive=false)
+  run(`cp liblinear.so.3 ../liblinear-weights.so.3`)
 end
