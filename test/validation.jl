72972c18ce4884f336e4bca3e13237ae80efd9d5
Thuener <thuener@gmail.com>
2019-02-18 10:15:13 -0300

    Add tests for weights

---
+++ test/validation.jl
@@ -0,0 +1,17 @@
+@testset "Validation" begin
+    model = linear_train(labels[1:2:end], inst[:, 1:2:end]; verbose=true, solver_type=Cint(0))
+    GC.gc()
+    (class, decvalues) = linear_predict(model, inst[:, 2:2:end], verbose=true)
+    correct = ones(Bool, length(class))
+    correct[[30,42,43,65,67]] .= false
+    @test (class .== labels[2:2:end]) == correct
+
+    @testset "Sparse matrix" begin
+        model = linear_train(labels[1:2:end], sparse(inst[:, 1:2:end]); verbose=true, solver_type=Cint(0))
+        GC.gc()
+        (class, decvalues) = linear_predict(model, sparse(inst[:, 2:2:end]), verbose=true)
+        correct = ones(Bool, length(class))
+        correct[[30,42,43,65,67]] .= false
+        @test (class .== labels[2:2:end]) == correct
+    end
+end
